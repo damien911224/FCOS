32d390a3f7503635f18a371dc16be9d17bb741d5
Ji.hwan Kim <damien911224@gmail.com>
2021-08-04 15:57:52 +0900

    Update test_net.py

--- tools/test_net.py
+++ tools/test_net.py
@@ -1,6 +1,8 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 # Set up custom environment before nearly anything else is imported
 # NOTE: this should be the first import (no not reorder)
+import sys
+sys.path.append("/mnt/hdd0/FCOS")
 from fcos_core.utils.env import setup_environment  # noqa F401 isort:skip
 
 import argparse
