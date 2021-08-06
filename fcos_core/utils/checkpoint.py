8539f52fb6fe3e70b0edeb7cda9ab8508c448ed4
damien911224 <damien911224@gmail.com>
2021-08-06 16:19:03 +0900

    modified

--- fcos_core/utils/checkpoint.py
+++ fcos_core/utils/checkpoint.py
@@ -61,9 +61,11 @@ class Checkpointer(object):
         checkpoint = self._load_file(f)
         self._load_model(checkpoint)
         if "optimizer" in checkpoint and self.optimizer:
+            print("optimizer!!!!!!!")
             self.logger.info("Loading optimizer from {}".format(f))
             self.optimizer.load_state_dict(checkpoint.pop("optimizer"))
         if "scheduler" in checkpoint and self.scheduler:
+            print("scheduler!!!!!!!")
             self.logger.info("Loading scheduler from {}".format(f))
             self.scheduler.load_state_dict(checkpoint.pop("scheduler"))
 
